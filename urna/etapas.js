d0b967b08bbe5e8d3a46098de8fb3eb3de613ae5
valmirbezerra19 <92387164+valmirbezerra19@users.noreply.github.com>
2021-11-01 20:59:09 -0300

    etapas js
    etapas

---
+++ urna/etapas.js
@@ -0,0 +1,124 @@
+let etapas = [
+                 {
+                    titulo: 'VEREADOR',
+                    numeros: 5,
+                    candidatos: [
+                                    {
+                                        numero: '11111',
+                                        nome: 'BOMBEIRO BABÃO',
+                                        partido: 'AAA',
+                                        fotos: [ {url:'11111.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                                    {
+                                        numero: '22222',
+                                        nome: 'FULANO DE TAL',
+                                        partido: 'ABC',
+                                        fotos: [ {url:'22222.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                                    {
+                                        numero: '33333',
+                                        nome: 'SALAFRARIO',
+                                        partido: 'TVC',
+                                        fotos: [ {url:'33333.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                                    {
+                                        numero: '44444',
+                                        nome: 'SORRISO',
+                                        partido: 'CUOL',
+                                        fotos: [ {url:'44444.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                                    {
+                                        numero: '55555',
+                                        nome: 'ADOLAR',
+                                        partido: 'ADF',
+                                        fotos: [ {url:'55555.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                                    {
+                                        numero: '66666',
+                                        nome: 'SAfA',
+                                        partido: 'CUOL',
+                                        fotos: [ {url:'66666.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                                    {
+                                        numero: '77777',
+                                        nome: 'JUQUINHA',
+                                        partido: 'PDTS',
+                                        fotos: [ {url:'77777.jpg', legenda: 'Vereador'}
+                                        ]
+                                    },
+                               ]
+                },
+
+                {
+                    titulo : 'PREFEITO',
+                    numeros: 2,
+                    candidatos: [
+                        {
+                            numero: '13',
+                            nome: 'ANGELA DO AMIN',
+                            partido: 'ABC',
+                            Vice:'GALEGO',
+                            fotos: [
+                                {url:'pr13.jpg', legenda: 'Prefeito'},
+                                {url:'pr13b.jpg', legenda: 'Vice-Prefeito', small: true}
+                            ]
+                        },
+                        {
+                            numero: '17',
+                            nome: 'BARBA',
+                            partido: 'ABCE',
+                            Vice:'BRUTOS BRAVO',
+                            fotos: [
+                                {url:'pr17.jpg', legenda: 'Prefeito'},
+                                {url:'pr17b.jpg', legenda: 'Vice-Prefeito', small: true}
+                            ]
+                        },
+                        {
+                            numero: '21',
+                            nome: 'FOQUINHA MANÉZINHO',
+                            partido: 'SOL',
+                            Vice:'NEXTRON',
+                            fotos: [
+                                {url:'pr21.jpg', legenda: 'Prefeito'},
+                                {url:'pr21b.jpg', legenda: 'Vice-Prefeito', small: true}
+                            ]
+                        },
+                        {
+                            numero: '22',
+                            nome: 'CARINHA',
+                            partido: 'GVT',
+                            Vice:'ZEZINHO DO MANGUE',
+                            fotos: [
+                                {url:'pr22.jpg', legenda: 'Prefeito'},
+                                {url:'pr22b.jpg', legenda: 'Vice-Prefeito', small: true}
+                            ]
+                        },
+                        {
+                            numero: '55',
+                            nome: 'GANSOLINO',
+                            partido: 'LAGOA',
+                            Vice:'PATO ZÉ',
+                            fotos: [
+                                {url:'pr55.jpg', legenda: 'Prefeito'},
+                                {url:'pr55b.jpg', legenda: 'Vice-Prefeito', small: true}
+                            ]
+                        },
+                        {
+                            numero: '77',
+                            nome: 'DONINHA CRESPA',
+                            partido: 'MINA',
+                            Vice:'ASTERIX',
+                            fotos: [
+                                {url:'pr77.jpg', legenda: 'Prefeito'},
+                                {url:'pr77b.jpg', legenda: 'Vice-Prefeito', small: true}
+                        ]
+                    },
+                ]
+            }
+        ];
---
+++ urna/script.js
@@ -0,0 +1,118 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero= '';
+let votoBranco = false;
+let votos = [];
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+    
+    let numeroHtml = '';
+            numero = '';
+            votoBranco = false;
+    for(let i=0;i<etapa.numeros;i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';
+                } else {
+                    numeroHtml += '<div class="numero"></div>';
+                }
+            }
+            seuVotoPara.style.display = 'none';
+            cargo.innerHTML = etapa.titulo;
+            descricao.innerHTML = '';
+            aviso.style.display = 'none';
+            lateral.innerHTML = '';
+            numeros.innerHTML = numeroHtml;
+}
+function atualizaInterface() {
+        let etapa = etapas[etapaAtual];
+        let candidato = etapa.candidatos.filter((item)=>{
+            if(item.numero === numero) {
+                return true;
+            } else {
+               return false;
+              }
+            });
+            if(candidato.length > 0) {
+                candidato = candidato[0];
+                seuVotoPara.style.display = 'block';
+                aviso.style.display = 'block';
+                descricao.innerHTML = `Nome: ${candidato.nome}<br/>PARTIDO: ${candidato.partido}`;
+                let fotosHml = '';
+                for (let i in candidato.fotos) {
+                if(candidato.fotos[i].small) {
+                fotosHml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+       } else {
+            fotosHml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+       }
+    }
+       lateral.innerHTML = fotosHml;
+    } else {
+           seuVotoPara.style.display = 'block';
+           aviso.style.display = 'block';
+           descricao.innerHTML= '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
+}
+function clicou(n) {
+        let elNumero = document.querySelector('.numero.pisca');
+        if(elNumero !== null) {
+            elNumero.innerHTML = n;
+            numero = `${numero}${n}`;
+            
+            elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+                atualizaInterface();
+     }
+   }
+ }
+function branco() {
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display ='block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = `<div class="aviso--grande pisca">VOTO EM BRANCO</div>`;
+    lateral.innerHTML = '';
+}
+function corrige() {
+ comecarEtapa();
+}
+function confirma(){
+  let etapa = etapas[etapaAtual];
+
+  let votoConfirmado = false;
+
+  if(votoBranco === true) {
+      votoConfirmado = true;
+      console.log("Confirmando como BRANCO...");
+      votos.push({
+          etapa: etapas[etapaAtual].titulo,
+          voto: 'branco'
+      });
+  }else if (numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+  if (votoConfirmado){
+      etapaAtual++;
+      if (etapas[etapaAtual] !== undefined){
+          comecarEtapa();
+      } else {
+          document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca"> FIM </div>';
+          console.log(votos);
+      }
+  }
+}
+
+comecarEtapa();
